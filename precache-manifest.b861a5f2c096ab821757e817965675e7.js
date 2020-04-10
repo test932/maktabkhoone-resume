c84ee0c48aa4369b8d6908396d6573730ec06400
test932 <azmayeshi.te@gmail.com>
2020-04-10 16:04:23 +0430

    Updates

---
+++ precache-manifest.b861a5f2c096ab821757e817965675e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4dd96582256d4588c2977b0996e5e8",
+    "url": "/maktabkhoone-resume/index.html"
+  },
+  {
+    "revision": "f9847df6961a937a9156",
+    "url": "/maktabkhoone-resume/static/css/main.b6eb6d98.chunk.css"
+  },
+  {
+    "revision": "6bfbc3ad98129d60d557",
+    "url": "/maktabkhoone-resume/static/js/2.14204c74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maktabkhoone-resume/static/js/2.14204c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9847df6961a937a9156",
+    "url": "/maktabkhoone-resume/static/js/main.b924a378.chunk.js"
+  },
+  {
+    "revision": "d285ece1d297883a0a46",
+    "url": "/maktabkhoone-resume/static/js/runtime-main.97422217.js"
+  }
+]);
